f5346c4e798dfd425d460eeca04b01b9eaaf054d
lgallo95 <lgallo2005@gmail.com>
2022-01-31 20:27:34 -0800

    removed logout endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -31,19 +31,5 @@ router.post("/login", (req, res, next) => {
     .catch(next);
 });
 
-router.get('/logout', (req, res) => {
-  if (req.session.username) {
-    req.session.destroy((err) => {
-      if (err) {
-        res.set('Set-Cookie', 'monkey=bar')
-        res.json({ message: `sorry, could you retry` })
-      } else {
-        res.json({ message: `bye, it was awesome` })
-      }
-    })
-  } else {
-    res.json({ message: `I do not believe we have met?` })
-  }
-})
 
 module.exports = router;
