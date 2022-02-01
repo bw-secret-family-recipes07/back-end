4e2528682eed2ad21b3f93c17a8c441d55b69c80
lgallo95 <lgallo2005@gmail.com>
2022-02-01 13:00:29 -0800

    testing get

--- api/items/items-router.js
+++ api/items/items-router.js
@@ -4,11 +4,13 @@ const Items = require('./items-model')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    Items.find()
-      .then(items => {
-        res.json(items)
-      })
-      .catch(next)
+try{
+const stuff = req.body
+console.log(stuff)
+}
+catch (err){
+  console.log(err)
+}
   })
 
   module.exports = router
