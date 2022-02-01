43548d721e0c7310527bf1861c7aec4f767316ef
lgallo95 <lgallo2005@gmail.com>
2022-02-01 10:08:42 -0800

    added seed

--- api/data/seeds/02-recipe.js
+++ api/data/seeds/02-recipe.js
@@ -6,9 +6,12 @@ exports.seed = async function (knex) {
         password: '$2a$08$CjOzAqkUXePlNyZCG6TKuubIY.MpjKqOdrV/W3178ah483kyEbeSe'
       })
       await knex("items")
-      .insert({
-        item_name: 'Burger',
-        user_id: 1
-      })
+      .insert(
+        {item_name: 'Burger',user_id: 1},
+        {item_name: 'Pizza',user_id: 2},
+        {item_name: 'Ice Cream',user_id: 3},
+        {item_name: 'Taco',user_id: 4},
+        {item_name: 'Burrito',user_id: 5},
+        )
   }
   
