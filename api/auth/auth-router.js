3a0eef06de756f299baa5b6a171bfda5f2b728f2
lgallo95 <lgallo2005@gmail.com>
2022-01-31 20:14:12 -0800

    login and register works!!!

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,33 +1,34 @@
-const bcrypt = require('bcryptjs')
-const makeToken = require('./auth-token-builder')
-const router = require('express').Router()
-const User = require('../users/users-model')
-const { BCRYPT_ROUNDS } = require('../secrets/index')
+const bcrypt = require("bcryptjs");
+const makeToken = require("./auth-token-builder");
+const router = require("express").Router();
+const User = require("../users/users-model");
+const { BCRYPT_ROUNDS } = require("../secrets/index");
 
+router.post("/register", (req, res, next) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, BCRYPT_ROUNDS);
+  user.password = hash;
+  User.add(user)
+    .then((saved) => {
+      res.status(201).json({ message: `Great to have you, ${saved.username}` });
+    })
+    .catch(next);
+});
 
-router.post('/register', (req, res, next) => {
-    let user = req.body
-    const hash = bcrypt.hashSync(user.password, BCRYPT_ROUNDS)
-    user.password = hash
-    User.add(user)
-      .then(saved => {
-        res.status(201).json({ message: `Great to have you, ${saved.username}` })
-      })
-      .catch(next) 
-  })
+router.post("/login", (req, res, next) => {
+  let { username, password } = req.body;
+  User.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = makeToken(user);
+        res
+          .status(200)
+          .json({ message: `Welcome back ${user.username}...`, token });
+      } else {
+        next({ status: 401, message: "Invalid Credentials" });
+      }
+    })
+    .catch(next);
+});
 
-  router.post('/login', (req, res, next) => {
-    let { username, password } = req.body
-    User.findBy({ username })
-      .then(([user]) => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          const token = makeToken(user)
-          res.status(200).json({ message: `Welcome back ${user.username}...`, token })
-        } else {
-          next({ status: 401, message: 'Invalid Credentials' })
-        }
-      })
-      .catch(next)
-  })
-
-module.exports = router
+module.exports = router;
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -14,18 +14,20 @@ function getAllUsers() {
 
 function find() {}
 
-function findBy(filter) { 
-  return db('users').where(filter)
-} 
+function findBy(filter) {
+  return db("users").where(filter);
+}
 
 async function add(user) {
-    const [username] = await db('users').insert(user, ['user_id', 'username', 'password'])
-    
-    return username
+  const [username] = await db("users").insert(user, [
+    "user_id",
+    "username",
+    "password",
+  ]);
+
+  return username;
 }
 
 function findById(user_id) {
-    return db("users")
-    .where({ user_id })
-    .first()
+  return db("users").where({ user_id }).first();
 }
