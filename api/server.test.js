4adeac501a3e23f7eb73d546abd8db19a7a2bb09
lgallo95 <lgallo2005@gmail.com>
2022-02-02 15:37:53 -0800

    register test done

--- api/server.test.js
+++ api/server.test.js
@@ -6,9 +6,11 @@ beforeAll(async () => {
     await db.migrate.rollback()
     await db.migrate.latest()
   })
+
   beforeEach(async () => {
     await db.seed.run()
   })
+
   afterAll(async () => {
     await db.destroy()
   })
@@ -20,6 +22,12 @@ beforeAll(async () => {
               .post('/api/auth/register')
               .send({ username: 'John', password: 'Wick'})
               expect(res.status).toBe(201)
+          })
+          test('Returns John Wick', async () => {
+            const res = await request(server)
+            .post('/api/auth/register')
+            .send({ username: 'John', password: 'Wick'})
+            expect(res.body.message).toBe("Great to have you, John")
           })
       })
   })
