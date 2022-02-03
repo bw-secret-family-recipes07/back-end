c595068f1598fe42bfc29e126c210c7a6d8e39dc
luisbryandr <luisbdominguezr@gmail.com>
2022-02-03 17:27:40 -0500

    delete id endpoint middleware checks existing id

--- api/items/items-router.js
+++ api/items/items-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Item = require("./items-model");
-const { validateItem } = require('./items-middleware');
+const { validateItem, verifyDeleteItem } = require('./items-middleware');
 
 const router = express.Router();
 
@@ -26,7 +26,38 @@ router.get('/item_name', restricted, (req, res, next) => {
   .catch(next)
 
 })
-  // const {id} = req.params
+ 
+
+
+router.post('/', restricted, validateItem, (req, res, next) => {
+  Item.add(req.body, req.decodedJwt.subject)
+      .then(newitem => {
+          res.status(201).json({message: `Item has been added`, newitem, info: req.decodedJwt.subject })
+      })
+      .catch(next)
+})
+
+
+
+router.put('/:id', restricted, validateItem, (req, res, next) => {
+    Item.edit(req.params.id, req.body)
+      .then(edited => {
+        res.json(edited)
+      })
+      .catch(next)
+})
+
+
+router.delete('/:id', verifyDeleteItem,(req, res, next) => {
+  Item.del(req.params.id)
+  .then(() => res.status(200).json({message:`item with id ${req.params.id} was sucessfully deleted`}))
+  .catch(() => res.status(500).json({message:`item with id ${req.params.id} was not able to be deleted`}))
+    
+})
+
+module.exports = router;
+
+ // const {id} = req.params
   // Item.findById(id)
   //     .then(item => {
   //         if (!item) {
@@ -53,33 +84,3 @@ router.get('/item_name', restricted, (req, res, next) => {
 
 // when new item posted needs to contain the current user_id
 // add a item verification if item already exists
-
-
-router.post('/', restricted, validateItem, (req, res, next) => {
-  Item.add(req.body, req.decodedJwt.subject)
-      .then(newitem => {
-          res.status(201).json({message: `Item has been added`, newitem, info: req.decodedJwt.subject })
-      })
-      .catch(next)
-})
-
-
-
-router.put('/:id', restricted, validateItem, (req, res, next) => {
-    Item.edit(req.params.id, req.body)
-      .then(edited => {
-        res.json(edited)
-      })
-      .catch(next)
-})
-
-
-router.delete('/:id', (req, res, next) => {
-  Item.del(req.params.id)
-  .then(() => res.status(200).json({message:`item with id ${req.params.id} was sucessfully deleted`}))
-  .catch(() => res.status(500).json({message:`item with id ${req.params.id} was not able to be deleted`}))
-    
-})
-
-module.exports = router;
-
