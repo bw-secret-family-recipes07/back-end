6e570e5ececc6b8143513ce70eff4a3b36bf5de5
lgallo95 <lgallo2005@gmail.com>
2022-02-04 09:28:26 -0800

    new test

--- api/server.test.js
+++ api/server.test.js
@@ -1,33 +1,29 @@
-const request = require('supertest')
-const server = require('./server.js')
-const db = require('../api/data/db-config')
+const request = require("supertest");
+const server = require("./server.js");
+const db = require("../api/data/db-config");
 
 beforeAll(async () => {
-    await db.migrate.rollback()
-    await db.migrate.latest()
-  })
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
 
-  beforeEach(async () => {
-    await db.seed.run()
-  })
+afterAll(async () => {
+  await db.destroy();
+});
 
-  afterAll(async () => {
-    await db.destroy()
-  })
-
-  describe('End Point Tests', () => {
-      describe('New user registration works', () => {        
-        test('Returns 200', async () => {
-              const res = await request(server)
-              .post('/api/auth/register')
-              .send({ username: 'John', password: 'Wick'})
-              expect(res.status).toBe(201)
-          })
-          test('Returns John Wick', async () => {
-            const res = await request(server)
-            .post('/api/auth/register')
-            .send({ username: 'John', password: 'Wick'})
-            expect(res.body.message).toBe("Great to have you, John")
-          })
-      })
-  })
+describe("End Point Tests", () => {
+  describe("New user registration works", () => {
+    test("Returns 200", async () => {
+      const res = await request(server)
+        .post("/api/auth/register")
+        .send({ username: "John", password: "Wick" });
+      expect(res.status).toBe(201);
+    });
+    test("Login Returns 200", async () => {
+      const res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "John", password: "Wick" });
+      expect(res.status).toBe(200);
+    });
+  });
+});
